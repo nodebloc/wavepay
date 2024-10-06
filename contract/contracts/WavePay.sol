a1d46ccc867a6c4085f185702f1f6ecd8a5d8f24
Kefas Kingsley <kefasiceking@gmail.com>
2024-10-06 06:51:20 +0100

    feat: Implemented saving multiple addresses and getting user saved addresses

--- contract/contracts/WavePay.sol
+++ contract/contracts/WavePay.sol
@@ -4,6 +4,19 @@ pragma solidity ^0.8.26;
 contract WavePay {
     address public owner;
 
+    struct Transfer {
+        address recipient;
+        uint256 amount;
+        uint256 timestamp;
+    }
+    
+    struct User {
+        address[] savedAddresses;
+        Transfer[] transferHistory;
+    }
+
+    mapping(address => User) private users;
+
     event AddressSaved(address indexed user, address[] savedAddresses);
     event AddressUpdated(address indexed user, address[] updatedAddresses);
     event MultiTransfer(address indexed sender, address[] recipients, uint256 totalAmount);
@@ -12,4 +25,13 @@ contract WavePay {
     constructor() {
         owner = msg.sender;
     }
+    function saveAddresses(address[] memory _addresses) public {
+        users[msg.sender].savedAddresses = _addresses;
+        emit AddressSaved(msg.sender, _addresses);
+    }
+
+    function getUserAddresses() external view returns (address[] memory) {
+        return users[msg.sender].savedAddresses;
+    }
+
 }
---
+++ contract/test/WavePay.ts
@@ -0,0 +1,56 @@
+import {
+    time,
+    loadFixture,
+  } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import hre from "hardhat";
+
+describe("WavePay", function () {
+  
+    async function deployWavePay() {
+  
+      const [owner, addr1, addr2, addr3, addr4, addr5, addr6] = await hre.ethers.getSigners();
+  
+      const WavePay = await hre.ethers.getContractFactory("WavePay");
+      const wavePay = await WavePay.deploy();
+  
+      return { wavePay, owner, addr1, addr2, addr3, addr4, addr5, addr6};
+    }
+  
+    describe("Deployment", function () {
+      it("Should deploy smart contract", async function () {
+        const { wavePay, owner } = await loadFixture(deployWavePay);
+        expect(await wavePay.getAddress()).to.not.equal("");
+      });
+
+      it("Should confirm owner address", async function () {
+        const { wavePay, owner, addr1, addr2 } = await loadFixture(deployWavePay);
+        expect(await wavePay.owner()).to.equal(owner.address);
+      });
+    });
+
+    describe("Functions", function () {
+        it("Should save addresses and emit AddressSaved event", async () => {
+            const { wavePay, owner, addr1, addr2 } = await loadFixture(deployWavePay);
+            const savedAddresses = [addr1.address, addr2.address];
+            
+            await expect(wavePay.saveAddresses(savedAddresses))
+              .to.emit(wavePay, "AddressSaved")
+              .withArgs(owner.address, savedAddresses);
+        });
+
+        it("Should get saved addresses", async () => {
+            const { wavePay, owner, addr1, addr2, addr3, addr4 } = await loadFixture(deployWavePay);
+            const savedAddresses = [addr1.address, addr2.address, addr3.address, addr4.address];
+            
+            await expect(wavePay.saveAddresses(savedAddresses))
+              .to.emit(wavePay, "AddressSaved")
+              .withArgs(owner.address, savedAddresses);
+            
+            const userAddresses = await wavePay.connect(owner).getUserAddresses();
+            console.log(userAddresses)
+            expect(userAddresses).to.deep.equal(savedAddresses);
+        });
+    });
+});
