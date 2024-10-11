976bf3a173dbc8763ec5f8f2df93a884e506f20e
dimka90 <dimkayilrit@gmail.com>
2024-10-11 16:46:08 +0100

    Added tv subcritption and airtime purchase

--- frontend/src/pages/Utilities.tsx
+++ frontend/src/pages/Utilities.tsx
@@ -1,174 +1,267 @@
-import React from 'react';
-import { ChevronDown, CodeSquare, Send } from 'lucide-react';
+import React, { useState, useEffect } from 'react';
+import { ChevronDown, Send } from 'lucide-react';
 import DashNav from '../components/DashNav';
-import  { useState, useEffect } from "react";
+
 interface UtilitiesProps {
   btcAmount?: number;
 }
+
 interface Category {
   identifier: string;
   name: string;
 }
 
-const network:Array<string> = ["mtn","airtel", "glo", "etisalat"]
-
 const Utilities: React.FC<UtilitiesProps> = ({ btcAmount = 0.1824 }) => {
-  const [categories, setCategories] = useState<Category[]>([]); // State for categories
+  const [categories, setCategories] = useState<Category[]>([]);
   const [selectedCategory, setSelectedCategory] = useState('');
-  const [selectedNetwork, setSelectedNetwork] = useState('');  
-  const [amount, setAmount] = useState('');  
-  const [phoneNumber, setNumber] = useState('');  
- 
+  const [selectedNetwork, setSelectedNetwork] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [phoneNumber, setNumber] = useState('');
+  const [meterNumber, setMeterNumber] = useState('');
+  const [subscriptionDetails, setSubscriptionDetails] = useState('');
+  const [tvSubscriptionPlans, setTvSubscriptionPlans] = useState<{ name: string, variation_code: string, variation_amount: string }[]>([]);
+  const [selectedProvider, setSelectedProvider] = useState('');
+  const [variationCodes, setVariationCodes] = useState<{ code: string, amount: string }[]>([]);
+  const [cardNumber, setCardNumber] = useState(''); 
+  const [selectedVariationAmount, setSelectedVariationAmount] = useState<number | null>(null); 
 
   useEffect(() => {
-    const fetchBalance = async () => {
+    const fetchCategories = async () => {
       try {
-        // Fetching balance from the backend
         const response = await fetch("http://localhost:5000/api/vtpass-categories");
-        // console.log(response)
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
+        if (!response.ok) throw new Error("Network response was not ok");
         const data = await response.json();
-        
         setCategories(data.content);
       } catch (error) {
-        console.error("Error fetching balance:", error); // Handle any errors
+        console.error("Error fetching categories:", error);
       }
     };
+    fetchCategories();
+  }, []);
 
-    fetchBalance(); // Call the function when the component loads
-  }, [selectedCategory]);
+  const fetchCableVariationCodes = async (provider: string) => {
+    try {
+      const response = await fetch(`http://localhost:5000/api/get-cable-variation-code?cableType=${encodeURIComponent(provider)}`);
+      if (!response.ok) throw new Error('Failed to fetch subscription plans');
+
+      const data = await response.json();
+      setTvSubscriptionPlans(data.content.varations);
+      setVariationCodes(data.content.varations.map((variation) => ({ code: variation.variation_code, amount: variation.variation_amount })));
+    } catch (error) {
+      console.error('Error fetching cable variation codes:', error);
+    }
+  };
+
+  const handleProviderChange = (provider: string) => {
+    setSelectedProvider(provider);
+    fetchCableVariationCodes(provider);
+  };
 
   const handlePayment = async () => {
-    if (!selectedCategory || !selectedNetwork || !amount) {
-      alert("Please select a category, network, and enter an amount.");
+    if (!selectedCategory || (amount <= 0 && selectedCategory !== 'tv-subscription')) {
+      alert('Please select a category and enter a valid amount.');
       return;
     }
 
-    const paymentData = {
-      // category: selectedCategory,
-      serviceId: selectedNetwork,
-      phone:phoneNumber,
-      amount: parseFloat(amount),
+    const paymentData: Record<string, any> = {
+      serviceID: selectedProvider,
+      amount: selectedCategory === 'tv-subscription' ? selectedVariationAmount : amount, 
     };
 
+    if (selectedCategory === 'airtime') {
+      paymentData.phone = phoneNumber;
+      paymentData.network = selectedNetwork;
+      paymentData.serviceID= selectedNetwork;
+    } else if (selectedCategory === 'electricity') {
+      paymentData.meterNumber = meterNumber;
+    } else if (selectedCategory === 'tv-subscription') {
+      paymentData.provider = selectedProvider;
+      paymentData.plan = subscriptionDetails;
+      // Adding card number to payment data for TV subscription
+      paymentData.cardNumber = cardNumber;
+      paymentData.phone = phoneNumber;
+      // Also include billersCode and variation_code for the TV subscription
+      paymentData.billersCode = cardNumber;
+      paymentData.variation_code = subscriptionDetails; 
+    }
+
     try {
-      const response = await fetch("http://localhost:5000/api/vtpass-purchase", {
-        method: "POST",
+      console.log(paymentData)
+      const response = await fetch("http://localhost:5000/api/vtpass-cablesubscription", {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(paymentData),
       });
 
-      if (!response.ok) {
-        throw new Error("Payment processing failed.");
-      }
-
+      // console.log(response)
+      if (!response.ok) throw new Error('Payment processing failed.');
       const result = await response.json();
-      console.log(result)
-      // Handle the response as needed
-      if(result.code === "000")
-      {
-      alert(`Payment Successful: ${result.response_description}`);
+      console.log(result.code)
+      if (result.data.code === '000') {
+        alert(`Payment Successful: ${result.data.response_description}`);
+        // Reset all fields
+        setSelectedCategory('');
+        setAmount(0);
+        setNumber('');
+        setMeterNumber('');
+        setSelectedNetwork('');
+        setSelectedProvider('');
+        setSubscriptionDetails('');
+        setCardNumber(''); 
+        setSelectedVariationAmount(null);
       }
-      // Optionally reset the form
-      setSelectedCategory('');
-      setSelectedNetwork('');
-      setAmount('');
     } catch (error) {
-      console.error("Error processing payment:", error);
-      alert("Payment failed. Please try again.");
+      console.error('Error processing payment:', error);
+      alert('Payment failed. Please try again.');
+    }
+  };
+
+  const renderCategorySpecificFields = () => {
+    switch (selectedCategory) {
+      case 'airtime':
+        return (
+          <>
+            <select
+              value={selectedNetwork}
+              onChange={(e) => setSelectedNetwork(e.target.value)}
+              className="w-full bg-primary-600 border border-blue-500 rounded-full p-3 appearance-none"
+            >
+              <option value="">Select Network</option>
+              {['mtn', 'airtel', 'glo', 'etisalat'].map((net) => (
+                <option key={net} value={net}>
+                  {net}
+                </option>
+              ))}
+            </select>
+
+            {selectedNetwork && (
+              <input
+                type="text"
+                value={phoneNumber}
+                onChange={(e) => setNumber(e.target.value)}
+                placeholder="Enter Phone Number"
+                className="w-full bg-transparent border-b border-blue-500 pb-2 focus:outline-none mt-4"
+              />
+            )}
+
+            <input
+              type="number"
+              value={amount}
+              onChange={(e) => setAmount(parseFloat(e.target.value))}
+              placeholder="Enter Amount"
+              className="w-full bg-transparent border-b border-blue-500 pb-2 focus:outline-none mt-4"
+            />
+          </>
+        );
+      case 'electricity':
+        return (
+          <>
+            <input
+              type="text"
+              value={meterNumber}
+              onChange={(e) => setMeterNumber(e.target.value)}
+              placeholder="Enter Meter Number"
+              className="w-full bg-transparent border-b border-blue-500 pb-2 focus:outline-none mt-4"
+            />
+          </>
+        );
+      case 'tv-subscription':
+        return (
+          <>
+            <select
+              value={selectedProvider}
+              onChange={(e) => handleProviderChange(e.target.value)}
+              className="w-full bg-primary-600 border border-blue-500 rounded-full p-3 appearance-none"
+            >
+              <option value="">Choose Cable Provider</option>
+              {['dstv', 'startimes', 'gotv'].map((provider) => (
+                <option key={provider} value={provider}>
+                  {provider.toUpperCase()}
+                </option>
+              ))}
+            </select>
+
+            {selectedProvider && variationCodes.length > 0 && (
+              <select
+                value={subscriptionDetails}
+                onChange={(e) => {
+                  const selectedCode = variationCodes.find(v => v.code === e.target.value);
+                  if (selectedCode) {
+                    setSubscriptionDetails(selectedCode.code);
+                    setSelectedVariationAmount(parseFloat(selectedCode.amount)); // Set amount to the selected variation amount
+                  }
+                }}
+                className="w-full bg-primary-600 border border-blue-500 rounded-full p-3 appearance-none mt-4"
+              >
+                <option value="">Choose Variation Code</option>
+                {variationCodes.map(({ code, amount }) => (
+                  <option key={code} value={code}>
+                    {code} - N{amount}
+                  </option>
+                ))}
+              </select>
+            )}
+            {selectedVariationAmount !== null && ( 
+              <input
+                type="text"
+                value={`N${selectedVariationAmount}`}
+                readOnly
+                className="w-full bg-transparent border-b border-blue-500 pb-2 focus:outline-none mt-4"
+              />
+            )}
+            {subscriptionDetails && ( 
+              <input
+                type="text"
+                value={cardNumber}
+                onChange={(e) => setCardNumber(e.target.value)}
+                placeholder="Enter Card Number"
+                className="w-full bg-transparent border-b border-blue-500 pb-2 focus:outline-none mt-4"
+              />
+            )}
+              <input
+                type="text"
+                value={phoneNumber}
+                onChange={(e) => setNumber(e.target.value)}
+                placeholder="Enter Phone Number"
+                className="w-full bg-transparent border-b border-blue-500 pb-2 focus:outline-none mt-4"
+              />
+          </>
+        );
+      default:
+        return null;
     }
   };
 
   return (
-    <div className='bg-primary-100 text-white h-screen w-full'>
+    <div className="bg-primary-100 text-white h-screen w-full">
       <DashNav />
       <div className="flex items-center justify-center p-8">
-        {/* Increased the max-w-lg for a larger card */}
         <div className="w-[700px] bg-primary-500 px-8 py-10 mx-auto rounded-2xl shadow-lg">
           <h1 className="text-4xl font-bold mb-4 text-center">Pay Utilities</h1>
-          
+
           <div className="space-y-6">
-            <div className="relative">
-              <select 
-              value={selectedCategory} 
+            <select
+              value={selectedCategory}
               onChange={(e) => setSelectedCategory(e.target.value)}
-              className="w-full bg-primary-600 border border-blue-500 rounded-full p-3 appearance-none">
-                {/* <option value="">Who to pay</option>
-                <option value="">Who to pay</option>
-                <option value="">Who to pay</option> */}
-                <option value="">Who to pay</option>
-                {categories.map((category) => (
-                  <option key={category.identifier} value={category.identifier}>
-                    {category.name}
-                  </option>
-                ))}
-               
-              </select>
-             
-              <ChevronDown className="absolute right-3 top-1/2 transform -translate-y-1/2 text-blue-500" size={20} />
-              
-            </div>
-            {/* console.log({selectedCategory}) */}
-            <div>
-              {selectedCategory === 'airtime' ? (
-               <div className="relative">
-               <select 
-                 value={selectedNetwork} 
-                 onChange={(e) => setSelectedNetwork(e.target.value)}
-                 className="w-full bg-primary-600 border border-blue-500 rounded-full p-3 appearance-none"
-               >
-                 <option value="">Select Network</option>
-                 {network.map((net) => (
-                   <option key={net} value={net}>
-                     {net}
-                   </option>
-                 ))}
-               </select>
-               <div>
-                  <label className="block text-gray-400 mb-2">Amount:</label>
-                  <input 
-                    type="text" 
-                    value={amount} 
-                    onChange={(e) => setAmount(e.target.value)} // Control the input
-                    className="w-full bg-transparent border-b border-blue-500 pb-2 focus:outline-none" 
-                  />
-                  
-                  <label className="block text-gray-400 mb-2">Phone Number:</label>
-                  <input 
-                    type="text" 
-                    value={phoneNumber} 
-                    onChange={(e) => setNumber(e.target.value)} // Control the input
-                    className="w-full bg-transparent border-b border-blue-500 pb-2 focus:outline-none" 
-                  />
-                </div>
-               {/* console.log({selectedNetwork}) */}
-               {/* <ChevronDown className="absolute right-3 top-1/2 transform -translate-y-1/2 text-blue-500" size={20} /> */}
-             </div>): null }
-              {/* <label className="block text-gray-400 mb-2">Amount:</label>
-              <input type="text" className="w-full bg-transparent border-b border-blue-500 pb-2 focus:outline-none" />
-             */}
-             {/* console.log({amount}) */}
-             </div>
+              className="w-full bg-primary-600 border border-blue-500 rounded-full p-3 appearance-none"
+            >
+              <option value="">Select Category</option>
+              {categories.map((category) => (
+                <option key={category.identifier} value={category.identifier}>
+                  {category.name}
+                </option>
+              ))}
+            </select>
 
-            {/* <div className="relative">
-              <div className="w-full bg-primary-600 border border-blue-500 rounded-full p-3 flex justify-between items-center">
-                <span>{btcAmount.toFixed(4)}</span>
-                <div className="flex items-center">
-                  <img src="/api/placeholder/24/24" alt="Bitcoin" className="w-6 h-6 mr-2" />
-                  <span className="mr-6">BTC</span>
-                  <ChevronDown className="absolute right-3 top-1/2 transform -translate-y-1/2 text-blue-500" size={20} />
-                </div>
-              </div>
-            </div> */}
+            {renderCategorySpecificFields()}
 
-            <button onClick={handlePayment} 
-            className="bg-gradient-to-r from-blue-500 to-blue-700 border-2 border-white text-white w-full py-3 rounded-full flex items-center justify-center mt-8">
-              <Send size={20} className="mr-2" />
-              Pay now
+            <button
+              onClick={handlePayment}
+              className="w-full flex items-center justify-center bg-blue-700 text-white p-4 rounded-full transition duration-200 hover:bg-blue-600"
+            >
+              <Send className="mr-2" /> Pay
             </button>
           </div>
         </div>
@@ -178,3 +271,4 @@ const Utilities: React.FC<UtilitiesProps> = ({ btcAmount = 0.1824 }) => {
 };
 
 export default Utilities;
+
